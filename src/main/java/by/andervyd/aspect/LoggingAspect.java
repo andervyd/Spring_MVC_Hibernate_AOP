b468d8d006c3e656289b3967a73b226ecb545ee9
andervyd <andervyd@gmail.com>
2021-01-10 08:38:04 +0200

    added AOP, end course

---
+++ src/main/java/by/andervyd/aspect/LoggingAspect.java
@@ -0,0 +1,29 @@
+package by.andervyd.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Around("execution(* by.andervyd.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(
+            ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        MethodSignature methodSignature =
+                (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println("Begin of " + methodName);
+
+        Object targenMethodResult = proceedingJoinPoint.proceed();
+
+        System.out.println("End of " + methodName);
+
+        return targenMethodResult;
+    }
+}
